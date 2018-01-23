7b34d72f68cbe9313204e1e9fc9c6bf434f6cab1
Wayne Buxton <waynebuxton2184@yahoo.com>
2018-01-23 20:54:55 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if @status = "reversed"
+    if @status != "reversed"
       sender.balance += amount
       receiver.balance -= amount
     else
